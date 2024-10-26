4079f326e8a6315087ffb2532bfb16315172be93
shalini <shaini.appscomp@gmail.com>
2024-10-26 18:45:53 +0530

    [update]gif

--- apps_employee_company_transfer/__manifest__.py
+++ apps_employee_company_transfer/__manifest__.py
@@ -22,7 +22,7 @@
     ],
     # 'images': ['static/description/banner.png'],
     'images': ['static/description/gif.gif'],
-    'price': '42.13',
+    'price': '42.11',
     # 'price': '75',
     "license": 'OPL-1',
     'installable': True,
